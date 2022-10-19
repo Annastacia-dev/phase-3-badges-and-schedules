281534036964cd121f0a641bbb5f95e515a9988c
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-10-19 17:16:26 +0300

    completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index{|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|badge| puts badge}
+  assign_rooms(attendees).each{|assignment| puts assignment}
+end
